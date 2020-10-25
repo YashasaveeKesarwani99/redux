409cd13a4b26a958b6dbab1eee0dfc62ab613fb4
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-10-25 10:17:24 +0530

    Initial commit

---
+++ redux-basics.js
@@ -0,0 +1,29 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
+const initialState = {
+  sum: 0
+};
+//Reducers
+const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNT") {
+    state = {
+      ...state,
+      sum: state.sum + action.payload
+    };
+  }
+  if (action.type === "TEN_COUNT") {
+    state = {
+      ...state,
+      sum: state.sum + action.payload
+    };
+  }
+  return state;
+};
+//Store
+const store = createStore(rootReducer);
+//Dispatching actions
+store.dispatch({ type: "INC_COUNT", payload: 1 });
+store.dispatch({ type: "TEN_COUNT", payload: 10 });
+//Subscription
